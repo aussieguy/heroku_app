866dc982b1b4fdfc495266e5c0425b6f30846ad4
aussieguy <glen.hobby@visionaustralia.org>
2014-01-25 17:11:55 +1100

    Complete static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let (:base_title) { "Sample Application" }
+	
 	describe "Home page" do
-		it "should have the content 'Sample App'" do
+		it "should have the content 'sample application'" do
 			visit '/static_pages/home'
-			expect(page).to have_content('Sample App')
+			expect(page).to have_content('sample application')
+		end
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("#{base_title} | Home")
 		end
 	end
 	
@@ -14,12 +20,31 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("#{base_title} | Help")
+		end
 	end
 
 	describe "About page" do
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
+		end
+		it "should have the title 'About'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("#{base_title} | About Us")
+		end
+	end
+	
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact")
 		end
 	end
 	
